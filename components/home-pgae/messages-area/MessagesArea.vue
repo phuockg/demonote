ea49484bc2a3b5863f93300f29f40fde88246b5b
nguyen phuoc <quangphuoc@jv-it.com.vn>
2020-11-19 02:13:41 +0700

    create demo stream

--- components/home-pgae/messages-area/MessagesArea.vue
+++ components/home-pgae/messages-area/MessagesArea.vue
@@ -10,46 +10,32 @@
             </div>
             <ul class="messages-chat__container__frames-chat">
                 <li v-for="(item, index) in commnetUsers" :key="index" class="messages-chat__container__frames-chat__inbox-item">
-                    <img :src="item.avatar" alt="">
-                    <div class="messages-chat__container__frames-chat__inbox-item__content" >
-                      {{item.content}}
+                    <img class="messages-chat__container__frames-chat__inbox-item__avatar" :src="item.avatar" alt="">
+                    <div class="messages-chat__container__frames-chat__inbox-item__content">
+                      {{item.isShowGift ? item.contentGift:item.content}}
                     </div>
-                    <img v-if="item.isShowGift" :src="item.gift" alt="">
+                    <img class="messages-chat__container__frames-chat__inbox-item__gift"  v-if="item.isShowGift" :src="item.gift" alt="">
                 </li>
-             
             </ul>
             <div class="messages-chat__container__content-message">
                 <div class="messages-chat__container__content-message__btn-point">
                     <button @click="showIcon"><img :src="imgGift" alt=""></button>
                 </div>
                 <div class="messages-chat__container__content-message__input">
-                    <input v-on:keyup="enterText" type="text" :value="commnetUser.content">
+                    <!-- <input v-on:keyup="enterText" type="text" :value="commnetUser.content"> -->
+                    <span contenteditable="true"> nguyen quang phuoc</span>
                     <div class="messages-chat__container__content-message__input__btn-send">
                         <button @click="sendCommnent">send</button>
                     </div>
                 </div>
             </div>
             <ul v-if="isShowIcon"  class="messages-chat__container__icon">
-                <li @click="addIcon" class="messages-chat__container__icon__item">
-                    <img :src="imgSmile" alt="">
-                </li>
-                <li @click="addIcon" class="messages-chat__container__icon__item">
-                    <img :src="imgSmile" alt="">
-                </li>
-                <li @click="addIcon" class="messages-chat__container__icon__item">
-                    <img :src="imgSmiling" alt="">
-                </li>
-                <li @click="addIcon" class="messages-chat__container__icon__item">
-                    <img :src="imgSmiling" alt="">
-                </li>
-                <li @click="addIcon" class="messages-chat__container__icon__item">
-                    <img :src="imgSmile" alt="">
-                </li>
-                <li @click="addIcon" class="messages-chat__container__icon__item">
-                    <img :src="imgSmiling" alt="">
-                </li>
-                <li @click="addIcon" class="messages-chat__container__icon__item">
-                    <img :src="imgSmile" alt="">
+                <li v-for="(item, index) in dataGift"  :key="index" @click="()=>addIcon(item.img)" class="messages-chat__container__icon__item">
+                    <img class="messages-chat__container__icon__item__gift" :src="item.img" alt="">
+                    <div class="messages-chat__container__icon__item__infor"> 
+                        <p class="messages-chat__container__icon__item__infor__price">{{item.price}}</p>
+                        <img class="messages-chat__container__icon__item__infor__unit" :src="imgDiamond" alt="">
+                    </div>
                 </li>
             </ul>
       </div>
@@ -61,10 +47,27 @@ import imgUser from "../../../assets/images/user.svg";
 import imgSmile from "../../../assets/images/smile.svg";
 import imgSmiling from "../../../assets/images/smiling.svg";
 import imgGift from "../../../assets/images/gift-box.svg";
+import imgDiamond from "../../../assets/images/diamond.svg";
+//xe
+import imgBus from "../../../assets/images/bus.svg";
+import imgCar from "../../../assets/images/car.svg";
+//nươc
+import imgCola from "../../../assets/images/cola.svg";
+import imgDrinkWater from "../../../assets/images/drink-water.svg";
+//hoa
+import imgFlowerPot from "../../../assets/images/flower-pot.svg";
+import imgRose from "../../../assets/images/rose.svg";
+//trưng
+import imgEgg from "../../../assets/images/egg.svg";
+import imgEggs from "../../../assets/images/eggs.svg";
+//rong
+import imgDragon from "../../../assets/images/dragon.svg";
+import imgDragon2 from "../../../assets/images/dragon2.svg";
 let commnetUsers = [
     {
         name:"A1",
         content:"hello",
+        contentGift:"xin chao! `Name` vừa tặng bạn món quà ",
         avatar:imgUser,
         isShowGift:false
         
@@ -72,10 +75,44 @@ let commnetUsers = [
     {
         name:"A2",
         content:"Xin chào",
+        contentGift:"xin chao! `Name` vừa tặng bạn món quà ",
         avatar:imgUser,
         isShowGift:false
     }
     ]
+let dataGift = [{
+        img:imgFlowerPot,
+        price:1,
+    },{
+        img:imgRose,
+        price:5,
+    },{
+        img:imgCola,
+        price:10,
+    },{
+        img:imgDrinkWater,
+        price:15,
+    },{
+        img:imgCar,
+        price:100,
+    },{
+        img:imgBus,
+        price:400,
+    },{
+        img:imgEgg,
+        price:1000,
+    },{
+        img:imgEggs,
+        price:5000,
+    },{
+        img:imgDragon,
+        price:10000,
+    },{
+        img:imgDragon2,
+        price:20000,
+    }
+    
+]
 export default {
     name:"messages chat",
     data:()=>{
@@ -84,12 +121,26 @@ export default {
             imgSmile,
             imgSmiling,
             commnetUsers,
+            imgDiamond,
+            imgGift,
+            imgBus,
+            imgCar,
+            imgCola,
+            imgDrinkWater,
+            imgFlowerPot,
+            imgRose,
+            imgEgg,
+            imgEggs,
+            imgDragon,
+            imgDragon2,
+            dataGift,
             commnetUser:{
                 name:"A2",
-                content:"xin chao A2",
+                content:"",
+                contentGift:"xin chao! `Name` vừa tặng bạn món quà ",
                 avatar:imgUser,
                 gift:imgSmiling,
-                isShowGift:true
+                isShowGift:false
             },
             isShowIcon:false
         }
@@ -98,14 +149,15 @@ export default {
         enterText(e){
             this.commnetUser={...this.commnetUser,content:e.target.value,isShowGift:false}
         },
-        addIcon(){
-            this.commnetUser={...this.commnetUser, gift:imgSmiling, isShowGift:true}
+        addIcon(item){
+            this.commnetUser={...this.commnetUser, gift:item, isShowGift:true}
              this.commnetUsers=[...this.commnetUsers,this.commnetUser]
              this.isShowIcon=false
         },
         sendCommnent(){
+            this.commnetUser={...this.commnetUser,isShowGift:false}
             this.commnetUsers=[...this.commnetUsers,this.commnetUser]
-            this.commnetUser={...this.commnetUser,content:""}
+            this.commnetUser={...this.commnetUser,content:"",isShowGift:false}
             this.isShowIcon=false
         },
         
